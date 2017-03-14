372f6b82447331b13902b50de4cd72434d8255f2
unsad <wzxaylryw@hotmail.com>
2017-03-14 15:15:33 +0800

    更新瀑布流组件

---
+++ src/components/waterfall/waterfall.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="vue-waterfall" :style="style">
+    <slot></slot>
+	</div>
+</template>
+
+<script>
+  export default {}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus" scoped>
+  .vue-waterfall
+    position: relative
+</style>
